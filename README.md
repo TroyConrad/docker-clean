9ac97c54c91a220f599490f42667aaf430e4b4e2
Troy Conrad <troy.conrad@lbox.com>
2015-11-04 14:40:44 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+# docker-clean
+Simple shell script that deletes any stopped containers, dangling images and orphaned volumes.
+
+Based on commands from http://blog.yohanliyanage.com/2015/05/docker-clean-up-after-yourself
+
+To install:
+
+$ git clone https://github.com/TroyDConrad/docker-clean.git && cd docker-clean && sudo ./install.sh
---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# install.sh v1.0.0 21Oct2015 (Great Scott!)
+
+# Linux or bust!
+osName=`uname`
+if [[ "$osName" != 'Linux' ]]; then
+	echo "Sorry, this script only works on Linux."
+	exit
+fi
+
+scriptName='docker-clean'
+installPath='/opt/$scriptname'
+binPath='/usr/local/bin/$scriptname'
+
+echo "Installing $scriptname..."
+
+mkdir -p $installPath
+rm -rf $installPath/*
+cp -r * $installPath
+ln -fs $installPath/$scriptname $binPath
+chmod ugo+rx $binPath
