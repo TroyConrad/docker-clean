155bdc3a2c65000cb1439055c6f5c7552752cbaf
Troy Conrad <troy.conrad@lbox.com>
2015-11-11 21:27:45 -0800

    docker-clean v0.2.0: Major update 	Check if docker is instaled and running 	Check if effective root 	Added -d dry run mode, which only shows what would be removed 	Added -h switch for help, -V for version info 	Disabled running docker-cleanup-volumes (will be re-enabled in future version)
    install.sh v0.0.3:
    	Run `docker-clean -V` after install

--- install.sh
+++ install.sh
@@ -1,22 +1,26 @@
 #!/bin/sh
 
-# install.sh v1.0.0 21Oct2015 (Great Scott!)
+# install.sh v1.0.3 11Nov2015
 
-# Linux or bust!
-osName=`uname`
-if [[ "$osName" != 'Linux' ]]; then
-	echo "Sorry, this script only works on Linux."
-	exit
+# Only for the big kids
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root/sudo." 
+   exit 1
 fi
 
-scriptName='docker-clean'
-installPath='/opt/$scriptname'
-binPath='/usr/local/bin/$scriptname'
+scriptName="docker-clean"
+installPath="/opt/$scriptName"
+binPath="/usr/local/bin/$scriptName"
 
-echo "Installing $scriptname..."
+echo "Installing $scriptName..."
 
+rm -rf $installPath
 mkdir -p $installPath
-rm -rf $installPath/*
 cp -r * $installPath
-ln -fs $installPath/$scriptname $binPath
+ln -fs $installPath/$scriptName $binPath
 chmod ugo+rx $binPath
+
+$binPath -V
+
+echo "Installation complete.\n"
+
